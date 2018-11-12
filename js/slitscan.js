53eef69450d9a4d987da8af8a802c380e6a67207
Kiki Wu <kuan-yi.wu@sjsu.edu>
2018-11-12 14:32:25 -0800

    1. add slitscan.html: it works!! 2. add neon.html: it works but looks a little different form processing.

--- js/slitscan.js
+++ js/slitscan.js
@@ -2,12 +2,12 @@ var video;
 var y = 0; //start from the top
 var x = 0; //start from the left
 
-
 function setup() {
-    createCanvas(1024, 600);
+    createCanvas(windowWidth, windowHeight);
     pixelDensity(1);
     video = createCapture(VIDEO);
-    video.size(1024, 600);
+    video.size(windowWidth, windowHeight);
+    video.hide();
 }
 
 function draw() {
@@ -16,14 +16,13 @@ function draw() {
     var h = video.height;
 
     copy(video, 0, h/2, w, 20, 0, y, w, 20);//load y pixels
-	copy(video, w/2, 0, 2, h, x, 0, 2, h );//load x pixels
+    copy(video, w/2, 0, 2, h, x, 0, 2, h );//load x pixels
 
     y = y + 1;
-	x = x + 1;
-  
+    x = x + 1;
+
     if (y > height, x > width) {
-    y = 0; //start from the top
-    x = 0; //start from the left
+        y = 0; //start from the top
+        x = 0; //start from the left
     }
 }
-
